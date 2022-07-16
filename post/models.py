edea7331781337faa7c7dc63f7df1d4071778958
Javohir Elmurodov <elmurodovjavohir@gmail.com>
2022-07-16 17:48:55 +0500

    feat: all models

--- post/models.py
+++ post/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from helpers.models import BaseModel
+from author.models import Author
+
 # Create your models here.
 CREATED = "created"
 MODERATION = "moderation"
@@ -11,6 +13,18 @@ POST_STATUS = (
 )
 
 
+class Category(BaseModel):
+    title = models.CharField(max_length=128)
+    slug = models.CharField(max_length=128, unique=True)
+    icon = models.FileField(upload_to="category/")
+    post_count = models.IntegerField(default=0)
+
+
+class Tag(BaseModel):
+    title = models.CharField(max_length=128)
+    slug = models.CharField(max_length=128, unique=True)
+
+
 class Post(BaseModel):
     title = models.CharField(max_length=128, verbose_name='Nomi')
     slug = models.CharField(max_length=128, unique=True)
@@ -19,12 +33,14 @@ class Post(BaseModel):
     image = models.ImageField(upload_to="post/", null=True)
 
     author = models.ForeignKey(
-        'Author', on_delete=models.CASCADE, related_name='posts')
+        Author, on_delete=models.CASCADE, related_name='posts')
     category = models.ForeignKey(
-        'Category', on_delete=models.SET_NULL, null=True, related_name='posts')
-    tags = models.ManyToManyField('Tag', related_name='posts')
+        Category, on_delete=models.SET_NULL, null=True, related_name='posts')
+    tags = models.ManyToManyField(Tag, related_name='posts')
 
     published_date = models.DateField(null=True)
     status = models.CharField(max_length=15, choices=POST_STATUS)
     read_min = models.IntegerField(default=0)
     views_count = models.PositiveIntegerField(default=0)
+
+    is_popular = models.BooleanField(default=False)
