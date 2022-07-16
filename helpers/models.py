0bde7ea51bfbb6dec9096226afc8503e91f1a17b
Javohir Elmurodov <elmurodovjavohir@gmail.com>
2022-07-16 16:58:42 +0500

    feat: Post model"

---
+++ helpers/models.py
@@ -0,0 +1,7 @@
+from venv import create
+from django.db import models
+
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
